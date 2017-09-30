a77ecacbe879b96a4950a9c3e37545b9f832e426
Priyakant Patel <priyamjm@hotmail.com>
2017-09-30 18:31:29 -0500

    wip

--- windows.setup.bat
+++ windows.setup.bat
@@ -1,5 +1,5 @@
 echo "Makesure to run in Administrator mode"
 echo "***** set machine wide environment variables"
-setx /M WORMHOLE_CONFIG "C:\wormhole\wormhole-config.json"
+setx /M WORMHOLE_CONFIG "%~dp0\wormhole-config.json"
 echo ***** Please restart any visual studio or command console window *****
 pause
