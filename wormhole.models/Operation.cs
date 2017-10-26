68a1188a02b3533f2cd37102cabbbe68c056c62c
Priyakant Patel <priyamjm@hotmail.com>
2017-10-25 22:53:12 -0500

    wip

--- wormhole.models/Operation.cs
+++ wormhole.models/Operation.cs
@@ -17,4 +17,27 @@ namespace wormhole.models
         public string OperationId { get; set; }    // Can be null
         public string Description { get; set; }
     }
+
+
+    /* ** Need to verify - start ** */
+    public class RouteTable
+    {
+        public List<RouteRule> Rules { get; set; }
+    }
+
+    public class RouteRule
+    {
+        /// <summary>
+        /// *, get, put, post, delete, ...
+        /// </summary>
+        public string HttpVerb { get; set; }
+        /// <summary>
+        /// Example get album by id => /album/*
+        /// </summary>
+        public string PathExpression { get; set; }
+        public string BackendPath { get; set; }
+        public List<string> InboundPolicies { get; set; }
+        public List<string> OutboundPolicies { get; set; }
+    }
+    /* ** Need to verify - end ** */
 }
